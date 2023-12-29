e533be68df565a63eba1a51ce634d6726767fc41
Peter Granum <psj335@ku.dk>
2023-12-29 15:47:35 +0100

    Fixed an issue where the python code was sending a float or integer, but the .net driver was expecting a system.decimal data type. Wrote the simpleMain so it is now a functioning while loop with a GUI. Added an ultraSimpleMain which is a script style code without a GUI

--- photonicdrivers/Piezo_kpz101/piezoKPZ101.py
+++ photonicdrivers/Piezo_kpz101/piezoKPZ101.py
@@ -30,6 +30,31 @@ class PiezoKPZ101:
         self.device = KCubePiezo.CreateKCubePiezo(self.serialNo)
         self.device.Connect(self.serialNo)
 
+        # Get Device Information and display description
+        device_info = self.device.GetDeviceInfo()
+        print(device_info.Description)
+
+        # Start polling and enable
+        self.device.StartPolling(250)  #250ms polling rate
+        time.sleep(0.25)
+        # self.device.EnableDevice()
+        # time.sleep(0.25)  # Wait for device to enable
+
+        if not self.device.IsSettingsInitialized():
+            self.device.WaitForSettingsInitialized(10000)  # 10 second max wait timeout
+            assert self.device.IsSettingsInitialized() is True
+
+        # Load the device configuration - output not valid until after this call
+        self.device_config = self.device.GetPiezoConfiguration(self.serialNo)
+
+        # This shows how to obtain the device settings
+        self.device_settings = self.device.PiezoDeviceSettings
+
+        # Set the Zero point of the device
+        # print("Setting Zero Point")
+        # self.device.SetZero()
+
+
     def enable(self):
         try:
             self.device.EnableDevice()
@@ -37,6 +62,9 @@ class PiezoKPZ101:
             print("enable failed")
             return 1
         return 0
+    
+    def disable(self):
+        self.device.DisableDevice()
     
     def getConfig(self):
         device_config = self.device.GetPiezoConfiguration(self.serialNo)
@@ -47,13 +75,40 @@ class PiezoKPZ101:
         max_voltage = self.device.GetMaxOutputVoltage()
         return max_voltage
     
-    def setOutputVoltage(self, voltage):
-        self.device.SetOutputVoltage(voltage)
+    def setZero(self):
+        # set the output of the device to 0 V
+        self.device.SetZero()
+    
+    def setOutputVoltage(self, voltageString):
+        # print("setting voltage to [V]:")
+        # print(voltageString)
+        try:
+            voltageFloat = float(voltageString) 
+            # Now 'float_value' is guaranteed to be a float
+            # You can use 'float_value' in the rest of your function
+            # print(f"The input as a float: {voltageFloat}")
+        except ValueError:
+            print("Error: Input is not a valid float.")
+
+        self.device.SetOutputVoltage(Decimal(voltageFloat))
 
     def getOutputVoltage(self):
         output_voltage = self.device.GetOutputVoltage()
         return output_voltage
         
+    def getStatus(self):
+        statusBits = hex(self.device.GetStatusBits())
+        print(statusBits)
+        ENABLED_MASK = hex(0x80000000) # got this from the documentation. search for GetStatusBits
+        print(ENABLED_MASK)
+        is_enabled = (int(statusBits,16) & int(ENABLED_MASK,16)) != 0
+        print(f"logic = {int(statusBits,16) & int(ENABLED_MASK,16)}")
+        if is_enabled:
+            print("Device is enabled")
+        else:
+            print("Device is disabled")
+        return statusBits
+    
     def getSerialNumber(self):
         deviceInfo = self._getDeviceInfo()
         print(deviceInfo.Serialnumber)
@@ -65,8 +120,11 @@ class PiezoKPZ101:
         print(DeviceInfo)
         return DeviceInfo
 
-
-    def __del__(self):
-        # this is the destructor
-        print('destructing')
-        self.device.Disconnect()
+    # destructor
+    # it is not recommended to use destructors in most cases in python
+    # there is a question of wheter __del__ or __exit__ is the better method
+    # def __exit__(self):
+    #     # this is the destructor. using exit is more reliable than the del function, where timing is not guaranteed
+    #     print('destructing')
+    #     self.device.StopPolling()
+    #     self.device.Disconnect()
