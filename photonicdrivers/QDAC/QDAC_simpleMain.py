16c2e16d2a42561e963de54aabc6cbd28e383663
pgranum <peter.granum@gmail.com>
2024-04-17 11:58:05 -0800

    added a simple main for the QDAC

--- photonicdrivers/QDAC/QDAC_simpleMain.py
+++ photonicdrivers/QDAC/QDAC_simpleMain.py
@@ -1,7 +1,25 @@
-import qdac2
-import common.connection as conn
-# from serial import Serial
+# import qdac2
+# import common.connection as conn
+# # from serial import Serial
 
-device = conn.find_qdac2_on_usb()
-qdac = qdac2.QDAC2(device)
-print(qdac.status())
+# device = conn.find_qdac2_on_usb()
+# qdac = qdac2.QDAC2(device)
+# print(qdac.status())
+
+import socket
+
+IPAddress='10.209.67.125'
+Port=5025
+
+print('Connecting via ethernet')
+connectionType = 'Ethernet'
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.settimeout(5) # sets the timeout of the receive command. 
+server_address = (IPAddress, Port) #IP address, port
+sock.connect(server_address)
+
+commandString = "*IDN?\n"
+sock.sendall(commandString.encode())
+response = sock.recv(100)
