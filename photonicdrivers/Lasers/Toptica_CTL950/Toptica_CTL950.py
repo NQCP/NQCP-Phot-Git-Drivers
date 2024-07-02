fb6b0edacbc2e7e1390f2d8e8cc5442ca25551de
Magnus Linnet Madsen <fkr476@ku.dk>
2024-07-02 11:28:47 +0200

    updated the Toptica driver

--- photonicdrivers/Lasers/Toptica_CTL950/Toptica_CTL950.py
+++ photonicdrivers/Lasers/Toptica_CTL950/Toptica_CTL950.py
@@ -1,40 +1,21 @@
-import json
-
 import toptica.lasersdk.dlcpro.v3_0_1 as toptica
-from photonicdrivers.Instruments.Abstract.Instrument import Instrument
-from photonicdrivers.Instruments.Settings.Console_Controller import Console_Controller
 
-
-class Toptica_CTL950(Instrument):  # Developer: Magnus Linnet Madsen
+class Toptica_CTL950(Instrment):  # Developer: Magnus Linnet Madsen
 
     def get_id(self):
         return "toptica_dlcpro01"
 
-    def __init__(self, IP_address='10.209.67.103', settings_path="C:/Users/NQCPQP/PycharmProjects/LabController/Code/Settings/Toptica_CTL950/Toptica_CTL950_Settings.txt"):
-        self.settings_path = settings_path
-        self.is_diode_enabled = None
-        self.laser = toptica.DLCpro(toptica.NetworkConnection(IP_address))
-        self.power_mW_set = None
+    def __init__(self):
+        print("initalising laser")
 
     def __del__(self):
         self.disconnect()
 
-    def enable_emission(self):
-        self.enable_diode()
+    def connect(self, IP_address='10.209.67.103'):
+        self.laser = toptica.DLCpro(toptica.NetworkConnection(IP_address))
 
-    def disable_emission(self):
-        self.disable_diode()
-
-    def enable_diode(self):
-        self.is_diode_enabled = True
-        self.set_diode()
-
-    def disable_diode(self):
-        self.is_diode_enabled = False
-        self.set_diode()
-
-    def set_diode(self):
-        print(self.laser.laser1.dl.cc.enabled.set(self.is_diode_enabled))
+    def set_diode(self, enable_bool):
+        self.laser.laser1.dl.cc.enabled.set(enable_bool)
 
     def connect(self):
         """
@@ -56,14 +37,6 @@ class Toptica_CTL950(Instrument):  # Developer: Magnus Linnet Madsen
         """
         return self.laser.laser1.ctl.wavelength_act.get()
 
-    def get_wavelength_set(self):
-        """
-        Returns the wavelength [nm] of the laser
-        @rtype: float
-        @return: wavelength [nm]
-        """
-        return self.wavelength_set_nm
-
     def set_wavelength(self, wavelength_nm: float):
         """
         Set wavelength [nm] of the laser
@@ -78,15 +51,6 @@ class Toptica_CTL950(Instrument):  # Developer: Magnus Linnet Madsen
 
         self.laser.laser1.ctl.wavelength_set.set(float(wavelength_nm))
 
-        return None
-
-    def set_wavelength_set(self, wavelength_nm: float):
-        """
-        Set wavelength [nm] of the laser
-        @param wavelength_nm: wavelength of the laser [nm]
-        @return: None
-        """
-        self.wavelength_set_nm = wavelength_nm
         return None
 
     def get_power(self):
@@ -115,12 +79,6 @@ class Toptica_CTL950(Instrument):  # Developer: Magnus Linnet Madsen
         @return: emission status of type (True) if the laser is on and (False) if the laser is off.
         """
         return self.laser.emission.get()
-
-    def print_emission_status(self):
-        if self.get_emission_status():
-            print("Emission status: The Toptica laser is on and emitting light")
-        else:
-            print("Emission status: The Toptica laser is off and not emitting any light")
 
     def get_power_stabilization(self):
         """
@@ -194,35 +152,6 @@ class Toptica_CTL950(Instrument):  # Developer: Magnus Linnet Madsen
         """
         return
 
-    def save_settings(self) -> None:
-        try:
-            dictionary = self.get_settings()
-            Console_Controller.print_message(dictionary)
-            with open(self.settings_path, "w") as text_file:
-                json.dump(dictionary, text_file)
-            Console_Controller.print_message("Saved " + self.get_id() + " settings")
-        except Exception as error:
-            Console_Controller.print_message("Could not save " + self.get_id() + " settings")
-            Console_Controller.print_message(error)
-
-
-    def get_settings(self) -> dict:
-        return {
-            "id": self.get_id(),
-            "wavelength_nm_set": self.wavelength_nm_set,
-            "power_mW_set": self.power_mW_set,
-            "wavelength_nm": self.get_wavelength(),
-            "power_mW": self.get_power(),
-            "power_stabilization": self.get_power_stabilization()
-        }
-
-    def load_settings(self) -> dict:
-
-        with open(self.settings_path, "r") as text_file:
-            settings_dict = json.load(text_file)
-            self.set_wavelength(settings_dict["wavelength"])
-            self.set_power(settings_dict["power_mW_set"])
-            self.set_power_stabilization(settings_dict["power_stabilization"])
 
 if __name__ == "__main__":
     laser = Toptica_CTL950()
