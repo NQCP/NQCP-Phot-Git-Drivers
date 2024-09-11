17a7b050e00124e7e95e05c24663ec2460844ada
pgranum <peter.granum@gmail.com>
2024-09-11 17:51:01 +0200

    did some testing

--- photonicdrivers/Piezo_AttocubeAMC/Piezo_AttocubeAMC_ultraSimpleMain.py
+++ photonicdrivers/Piezo_AttocubeAMC/Piezo_AttocubeAMC_ultraSimpleMain.py
@@ -5,12 +5,14 @@ ip = "192.168.1.1"
 
 amc = AMC.Device(ip)
 amc.connect()
-print(amc.control.getPositionsAndVoltages())
+x, y, z, v1, v2, v3 = amc.control.getPositionsAndVoltages()
+print((x,y,z))
 
-x = 2659504.854 # in nm
-y = 3400544.614
-z = 3145000.0
+x = x + 10*1000
+# y = y + 120*1000
+# z = z + 1*1000
 
-amc.control.MultiAxisPositioning(1, 1, 1, x, y, z)
+
+amc.control.MultiAxisPositioning(1, 1, 0, x, y, z)
 print(amc.control.getStatusMovingAllAxes())
 amc.close()
