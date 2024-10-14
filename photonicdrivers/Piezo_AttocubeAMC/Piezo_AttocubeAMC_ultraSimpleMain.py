76773b77db8f8c1ca5c17f7ee406a2b6fdfe73ba
pgranum <peter.granum@gmail.com>
2024-10-14 11:23:50 +0200

    changed coordinates

--- photonicdrivers/Piezo_AttocubeAMC/Piezo_AttocubeAMC_ultraSimpleMain.py
+++ photonicdrivers/Piezo_AttocubeAMC/Piezo_AttocubeAMC_ultraSimpleMain.py
@@ -8,8 +8,8 @@ amc.connect()
 x, y, z, v1, v2, v3 = amc.control.getPositionsAndVoltages()
 print((x,y,z))
 
-# x = x + -1*1000
-# y = y + -2*1000
+x = x + -1*1000
+# y = y + -1*1000
 # z = 3_160_000 # nm
 # x = 1977418
 # y = 2878946
