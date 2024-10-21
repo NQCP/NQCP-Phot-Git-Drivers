f3c60fe555bfba93ee72a37b003f9deaa941701e
pgranum <peter.granum@gmail.com>
2024-10-21 13:04:00 +0200

    minor dev

---
+++ photonicdrivers/Magnets/Atto1000_VectorMagnet_ultraSimpleMain.py
@@ -0,0 +1,13 @@
+
+
+class Atto1000_VectorMagnet():
+    def __init__(self, B_max:float=2, Bx_max:float=2, By_max:float=2, Bz_max:float=5) -> None:
+        '''
+        Fields in units of Tesla
+        '''
+        ps_x = "driver"
+
+    def set_B(Bx, By, Bz) -> None:
+        pass
+
+
