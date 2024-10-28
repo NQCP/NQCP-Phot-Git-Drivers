66f9edc318d5a278ca18115b438f562ba7f38bea
pgranum <peter.granum@gmail.com>
2024-10-28 14:12:33 +0100

    added more functions

--- photonicdrivers/Piezo_AttocubeAMC/Piezo_AttocubeAMC_ultraSimpleMain.py
+++ photonicdrivers/Piezo_AttocubeAMC/Piezo_AttocubeAMC_ultraSimpleMain.py
@@ -8,16 +8,16 @@ amc.connect()
 x, y, z, v1, v2, v3 = amc.control.getPositionsAndVoltages()
 print((x,y,z))
 
-x = x + -1*1000
-# y = y + -1*1000
-# z = 3_160_000 # nm
+# x = x + -1*1000
+y = y + -1*1000
+# z = 3_157_000 # nm
 # x = 1977418
 # y = 2878946
 
 
 
-# amc.control.MultiAxisPositioning(1, 1, 0, x, y, z)
 amc.control.MultiAxisPositioning(1, 1, 0, x, y, z)
+# amc.control.MultiAxisPositioning(0, 0, 1, x, y, z)
 print(amc.control.getStatusMovingAllAxes())
 print((x,y,z))
 amc.close()
