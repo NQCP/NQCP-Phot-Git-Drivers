6740039786c61e794f88b8330cc8ba6c81560d47
pgranum <peter.granum@gmail.com>
2025-02-06 15:45:06 +0100

    tried the newest test script from attocube but it did not work

--- photonicdrivers/Attocube_ASC500/ASC500_ultraSimpleMain.py
+++ photonicdrivers/Attocube_ASC500/ASC500_ultraSimpleMain.py
@@ -21,44 +21,86 @@ asc500.base.startServer()
 print("Send Profile: ")
 asc500.base.sendProfile(binPath + 'afm.ngp')
 
-print("Set Data Enable: ")
-asc500.data.setDataEnable(1)
+
+###############################################################
+
+# # print("Set Data Enable: ")
+# # asc500.data.setDataEnable(1)
+
+# # print("Getting Scanner State: ")
+# # print(asc500.scanner.getScannerState())
+
+# # print("Getting Scanner absolute coor system: ")
+# # print(asc500.scanner.getScannerAbsolutCoordSystem())
+
+
+# # print("Getting Scanner Position: ")
+# # print(asc500.scanner.getPositionsXYZRel())
+
+# # time.sleep(3)
+
+# # print("start scanner: ")
+# # # print(asc500.scanner.startScanner())
+# # asc500.scanner.startScanner()
+
+# # print("Setting Scanner Position: ")
+# # new_position_m = [-10.0e-6, 0.0] # array is in pm, which is then convereted to m
+# # asc500.scanner.setPositionsXYRel(new_position_m)
+# # time.sleep(0.5)
+
+# # print("stop scanner: ")
+# # print(asc500.scanner.stopScanner())
 
 # print("Getting Scanner State: ")
 # print(asc500.scanner.getScannerState())
 
-# print("Getting Scanner absolute coor system: ")
-# print(asc500.scanner.getScannerAbsolutCoordSystem())
-
-
 # print("Getting Scanner Position: ")
 # print(asc500.scanner.getPositionsXYZRel())
 
-time.sleep(3)
+# # print("Disabling outputs") # SHOULD PERHAPS ALSO BE ENABLED??
+# # print(asc500.base.setOutputs(0))
 
-print("start scanner: ")
-# print(asc500.scanner.startScanner())
-asc500.scanner.startScanner()
+# # print("closing scanner: ")
+# # asc500.scanner.closeScanner()
 
-print("Setting Scanner Position: ")
-new_position_m = [-10.0e-6, 0.0] # array is in pm, which is then convereted to m
-asc500.scanner.setPositionsXYRel(new_position_m)
-time.sleep(0.5)
+# print("Stopping Server: ")
+# asc500.base.stopServer()
 
-print("stop scanner: ")
-print(asc500.scanner.stopScanner())
 
-print("Getting Scanner State: ")
-print(asc500.scanner.getScannerState())
 
-print("Getting Scanner Position: ")
+###############################################################
+
+
+
+# Activates the outputs of the ASC500. This is mandatory if you want to use the analog outputs of the ASC500 (DAC1-6 and SCAN)
+asc500.base.setOutputsWaiting(1)
+# As this might take a little, we wait until the outputs are on:
+while not asc500.base.getOutputStatus():
+    print('Waiting for outputs to switch on')
+    print(asc500.base.getOutputStatus())
+    time.sleep(1)
+
+# Set the scanner moving speed. Not mandatory.
+asc500.scanner.setPositioningSpeed(1e-6)
+
+# Set the scanner position (in m).
+asc500.scanner.setPositionsXYRel([10e-6, 15e-6])
+
+# Wait while scanner is moving.
+print('Scanner State: {}'.format(asc500.scanner.getScannerStateMoving()))
+while asc500.scanner.getScannerStateMoving():
+    time.sleep(0.5)
+
+#Print the new position:
 print(asc500.scanner.getPositionsXYZRel())
 
-print("Disabling outputs") # SHOULD PERHAPS ALSO BE ENABLED??
-print(asc500.base.setOutputs(0))
+#%% Close ASC500
+#-----------------------------------------------------------------------
+# Switching off the outputs will lead to the scanners falling back to [0, 0]
+asc500.base.setOutputsWaiting(0)
+while asc500.base.getOutputStatus():
+    print('Waiting for outputs to switch off')
+    time.sleep(1)
 
-print("closing scanner: ")
-asc500.scanner.closeScanner()
-
-print("Stopping Server: ")
+# stops the server (not mandatory)
 asc500.base.stopServer()
