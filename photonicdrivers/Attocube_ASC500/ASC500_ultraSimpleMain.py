934089c0e5c318e31dc8be7f1a29ef1d615c6551
pgranum <peter.granum@gmail.com>
2025-04-30 11:15:05 +0200

    added comments to code after debugging with Clemens from attocube

--- photonicdrivers/Attocube_ASC500/ASC500_ultraSimpleMain.py
+++ photonicdrivers/Attocube_ASC500/ASC500_ultraSimpleMain.py
@@ -18,9 +18,13 @@ asc500 = ASC500(binPath, dllPath)
 print("Starting Server: ")
 asc500.base.startServer()
 
+# loads the voltage to position (which is temperature dependent) calibration from file
+# the afm file is a standard calibration ngp file. I should use the one corresponding to my scanner. 
 print("Send Profile: ")
-asc500.base.sendProfile(binPath + 'afm.ngp')
+asc500.base.sendProfile(binPath + 'afm.ngp') # zou can point to an ngp file to a different path
 
+# set the temperature that you are working at here. 
+asc500.limits.setTemperature(4.0) # in Kelvin
 
 ###############################################################
 
@@ -73,15 +77,15 @@ asc500.base.sendProfile(binPath + 'afm.ngp')
 
 
 # Activates the outputs of the ASC500. This is mandatory if you want to use the analog outputs of the ASC500 (DAC1-6 and SCAN)
-asc500.base.setOutputsWaiting(1)
+asc500.base.setOutputsWaiting(1) # 1 = activate output and wait. standard wait time is 1000 ms (input argument is optional)
 # As this might take a little, we wait until the outputs are on:
 while not asc500.base.getOutputStatus():
     print('Waiting for outputs to switch on')
     print(asc500.base.getOutputStatus())
     time.sleep(1)
 
-# Set the scanner moving speed. Not mandatory.
-asc500.scanner.setPositioningSpeed(1e-6)
+# Set the scanner moving speed. Not mandatory, as there is a default value in the profile loaded earlier.
+asc500.scanner.setPositioningSpeed(100e-6) # speed in m/s. Max is 300 um/s, but this might return in "ringing"
 
 #Print the new position:
 print(asc500.scanner.getPositionsXYZRel())
@@ -89,18 +93,25 @@ print(asc500.scanner.getPositionsXYZRel())
 # Set the scanner position (in m).
 asc500.scanner.setPositionsXYRel([6e-6, 5e-6])
 
-# Wait a short time for communication to take place
-time.sleep(0.5)
+# Wait a short time for communication to take place (0.5 s seems like a long time to Clemens. Can probably be lower. like 0.05 s)
+time.sleep(0.05)
 
 # Wait while scanner is moving.
-print('Scanner State: {}'.format(asc500.scanner.getScannerStateMoving()))
-# while asc500.scanner.getScannerStateMoving():
+print('Scanner State: {}'.format(asc500.scanner.getScannerStateMoving())) # according to Clemens, the "state moving" refers to somethings else - not the scanner moving.
+# cases should be: 
+#
 count =1
 while count < 10:
-    print("Scanner is still moving")
-    print('Scanner State: {}'.format(asc500.scanner.getScannerStateMoving()))
-    time.sleep(0.5)
+# while asc500.scanner.getScannerStateMoving() == 0:
+    
+    print(count)
     count += 1
+    print("Scanner is still moving")
+    print(asc500.scanner.getScannerStateMoving())
+    # print('Scanner State: {}'.format(asc500.scanner.getScannerStateMoving()))
+    print(asc500.scanner.getPositionsXYZRel())
+    time.sleep(0.5)
+    
 
 #Print the new position:
 print(asc500.scanner.getPositionsXYZRel())
@@ -109,6 +120,7 @@ print(asc500.scanner.getPositionsXYZRel())
 #%% Close ASC500
 #-----------------------------------------------------------------------
 # Switching off the outputs will lead to the scanners falling back to [0, 0]
+# if you want the scanners to stay at the current position, just dont switch off the outputs
 asc500.base.setOutputsWaiting(0)
 while asc500.base.getOutputStatus():
     print('Waiting for outputs to switch off')
