9285ab19cad67dcb7c941633b978fd1e6326ac01
Arnulfjsn <arnulfdk@gmail.com>
2025-06-16 15:20:36 +0200

    Reverted Andor_kymera to initialize on connect so it is usable on the server

--- photonicdrivers/Spectrographs/Andor_Kymera.py
+++ photonicdrivers/Spectrographs/Andor_Kymera.py
@@ -22,8 +22,8 @@ class Andor_Kymera(Connectable):
 
     def connect(self):
         self.spectrograph = ATSpectrograph(userPath="C:\\Program Files\\Andor SDK\\Python\\pyAndorSpectrograph\\pyAndorSpectrograph\\libs\\Windows\\64")
-        #message = self.spectrograph.Initialize("")
-        #print("Function Initialize returned {}".format(self.spectrograph.GetFunctionReturnDescription(message, 64)[1]))
+        message = self.spectrograph.Initialize("")
+        print("Function Initialize returned {}".format(self.spectrograph.GetFunctionReturnDescription(message, 64)[1]))
 
     def get_serial_number(self):
         sn, status = self.get_serial_number_with_success()
