34dec99429e35d5127e00d35e6d0e1388e464fce
rodadrthomas <dtk791@ku.dk>
2025-08-15 11:20:39 +0200

    added KU ipadress to test

--- photonicdrivers/Power_Meters/mpm220_test.py
+++ photonicdrivers/Power_Meters/mpm220_test.py
@@ -1,6 +1,7 @@
 import socket
 
-ip = "192.168.1.161"
+# ip = "192.168.1.161" # default device IP
+ip = "10.209.69.171" # KU IT registered IP, MAC address 3C 6F 45 10 11 64
 port = 5000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
