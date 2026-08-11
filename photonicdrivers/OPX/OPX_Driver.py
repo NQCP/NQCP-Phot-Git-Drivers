5c2a65a93d7618edd5d80a3adeb946bc1d58d630
Laurits Piehorsch <laurits.piehorsch@nbi.ku.dk>
2026-08-11 14:22:22 +0200

    Update OPX_Driver to close other machines when opening a quantum machine and rename execute_program to execute_sequence

--- photonicdrivers/OPX/OPX_Driver.py
+++ photonicdrivers/OPX/OPX_Driver.py
@@ -37,7 +37,7 @@ class OPX_Driver(Connectable):
             return False
     
     def open_quantum_machine(self, config):
-        return self.driver.open_qm(config=config)
+        return self.driver.open_qm(config=config, close_other_machines=True)
     
     def is_quantum_machine_open(self):
         if len(self.driver.list_open_qms()) > 0:
@@ -71,7 +71,7 @@ class OPX_Driver(Connectable):
             program_step["duration"] = duration
         self.sequence_stack.append(program_step)
 
-    def execute_program(self):
+    def execute_sequence(self):
         with program() as sequence_program:
             for step in self.sequence_stack:
                 if "pulse" in step and "element" in step and "duration" in step:
@@ -103,4 +103,3 @@ class OPX_Driver(Connectable):
     
     def get_devices(self):
         return self.driver.get_devices()
-        
